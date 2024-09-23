ecc7b4a28b9760852243c5ca40cf3dba31a99d87
MarySerebryakova <maryserebriakova1410@gmail.com>
2024-09-23 13:31:11 +0500

    23.09

---
+++ variable.py
@@ -0,0 +1,5 @@
+home_work = 12
+hours = 1.5
+course_name = "Phyton"
+one_work = hours/home_work
+print("Курс:",course_name,"количество выполненных заданий",home_work,"количество затраченных часов",hours,"время на одно задание",one_work)
