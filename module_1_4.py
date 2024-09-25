b8a76694a97e103b9ac09ac516b402c8bae1c3a2
MarySerebryakova <maryserebriakova1410@gmail.com>
2024-09-25 23:01:44 +0500

    24.09

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("город")
+print (len(my_string))
+my_string = "я строка"
+print("я строка".upper())
+print("я строка".lower())
+print("я строка".replace (" ",""))
+print(my_string [0])
+print(my_string [7])
