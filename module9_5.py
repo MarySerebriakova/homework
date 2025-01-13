ed0c25e2c0ce5f34a78a5cfea31500a23351237c
MarySerebriakova <MarySerebriakova1410@gmail.com>
2025-01-13 09:17:57 +0500

    Add files via upload

---
+++ module9_5.py
@@ -0,0 +1,62 @@
+# Определяем пользовательское исключение
+class StepValueError(ValueError):
+    pass
+
+
+# Определяем класс Iterator
+class Iterator:
+    def __init__(self, start, stop, step=1):
+        # Проверка значения шага
+        if step == 0:
+            raise StepValueError('Шаг не может быть равен 0')
+
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.pointer = start
+
+    def __iter__(self):
+        # Сброс указателя на стартовое значение
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        # Проверка условия завершения итерации
+        if (self.step > 0 and self.pointer > self.stop) or (self.step < 0 and self.pointer < self.stop):
+            raise StopIteration
+
+        # Сохранение текущего значения для возврата
+        current_value = self.pointer
+        # Увеличиваем указатель на шаг
+        self.pointer += self.step
+        return current_value
+
+
+# Мы можем использовать следующий код для тестирования класса
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+for i in iter2:
+    print(i, end=' ')
+print()
+
+for i in iter3:
+    print(i, end=' ')
+print()
+
+for i in iter4:
+    print(i, end=' ')
+print()
+
+for i in iter5:
+    print(i, end=' ')
+print()
